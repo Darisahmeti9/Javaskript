082dd5f38516a4a2eb8b04c2c5d43592c873e902
darisahmeti9 <darisahmeti9@gmail.com>
2024-10-08 07:16:46 -0700

    dar

---
+++ moduli27/main.js
@@ -0,0 +1,12 @@
+var a = 10
+var b = 6
+var c = 15
+var d = 19
+
+console.log(a>b && c<d)
+
+console.log(a == b && c <d)
+
+var school = "Digital school"
+
+console.log('My school name is ${school')
